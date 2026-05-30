74dbdea12e32a7d1962ff97490f8d11dc2a2af21
tsehla42 <tsehla@duck.com>
2026-05-30 20:16:21 +0300

    feat: add ALLOWED_GROUP_IDS config option

--- src/config.py
+++ src/config.py
@@ -7,6 +7,9 @@ BOT_TOKEN = os.environ.get("BOT_TOKEN", "")
 ALLOWED_USER_IDS = [
     int(uid) for uid in os.environ.get("ALLOWED_USER_IDS", "").split(",") if uid.strip()
 ]
+ALLOWED_GROUP_IDS = [
+    int(gid) for gid in os.environ.get("ALLOWED_GROUP_IDS", "").split(",") if gid.strip()
+]
 DOWNLOAD_DIR = os.environ.get("DOWNLOAD_DIR", "/tmp/bot-downloads")
 MAX_FILE_SIZE = int(os.environ.get("MAX_FILE_SIZE", "50"))
 MAX_CONCURRENT_DOWNLOADS = int(os.environ.get("MAX_CONCURRENT_DOWNLOADS", "3"))
